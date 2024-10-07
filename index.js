2daa20d82880e58787421fa79ed1cc16fbbd5d2b
asifalam515 <asibulalam515@gmail.com>
2024-10-07 23:05:31 +0600

    sum query started

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// mongo db
+
+//
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.6tngyrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    const serviceDB = client.db("carDoctor");
+    const servicesCollection = serviceDB.collection("serviecs");
+    const bookingCollection = client.db("carDoctor").collection("bookings");
+    // api
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { _id: 0, title: 1, price: 1, service_id: 1, img: 1 },
+      };
+      const result = await servicesCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    // bookings
+    // i am not looking for a specific data or every data
+    // sum data => those fulfill requirement
+    app.get("/bookings", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      // jodi request er query er moddhe email ta thake
+      if (req.query?.email) {
+        // email field ke dhore query koro.req.query.email theke pawa email er upre base kore
+        query = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
